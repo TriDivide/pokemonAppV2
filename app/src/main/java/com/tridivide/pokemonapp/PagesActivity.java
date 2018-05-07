74d2fc24a20c05705d0462d3bab640750d348d16
TriDivide <liamsmitha@gmail.com>
2018-05-07 21:12:30 +0100

    - initial commit

---
+++ app/src/main/java/com/tridivide/pokemonapp/PagesActivity.java
@@ -0,0 +1,13 @@
+package com.tridivide.pokemonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pages_activity);
+    }
+}
